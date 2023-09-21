b86f3277dffe0ef35d68427d410d7d1b82f5b14b
samade747 <samad.e747@gmail.com>
2023-09-22 00:16:42 +0500

    last update

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ function signup(){
     var pass = document.getElementById('spass').value;
     localStorage.setItem('Email',email);
     localStorage.setItem('password',pass);
-    location.href = './signIn.html';
+    window.location.href = './signIn.html';
 }
 
 function signin(){
     var email = document.getElementById('lemail').value;
     var pass = document.getElementById('lpass').value;
     if(localStorage.getItem('Email') == email && localStorage.getItem('password') == pass){
-        location.href = './welcome.html'
+        window.location.href = './welcome.html'
     }
     else{
     alert('invalid user name password  ..');
-    location.href = './index.html'
+    window.location.href = './index.html'
     }
 }
 
